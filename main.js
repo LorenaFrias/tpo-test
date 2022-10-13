b3bb58dda066447d3584a24d95a87d2ac8c28a5b
Lorena Frias <lorenavfrias@gmail.com>
2022-10-12 23:48:00 -0300

    se agregó scroll up button

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ const observer = new IntersectionObserver(entries => {
 slides.forEach(slide => {
     observer.observe(slide)
 })
+
+// SCROLL UP BUTTON
+
+const scrollBtn = document.querySelector('.scrollup');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = 'block';
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+})
